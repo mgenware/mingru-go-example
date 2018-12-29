a54eec556d60a62a59dd5831d1a16c645f65eee0
Mgen <m@mgenware.com>
2018-12-30 02:40:36 +0800

    Upgrade deps

--- models/userTA.ts
+++ models/userTA.ts
@@ -1,21 +1,21 @@
-import * as dd from 'dd-models';
-import user from './user';
+import * as dd from "dd-models";
+import user from "./user";
 
 const userTA = dd.actions(user);
 // Select a user profile by ID
-userTA.select('UserProfile', user.id, user.name).byID();
+userTA.select("UserProfile", user.id, user.name).byID();
 // Select all user profiles
-userTA.selectAll('AllUserProfiles', user.id, user.name);
+userTA.selectAll("AllUserProfiles", user.id, user.name);
 // Select a single field by ID
-userTA.selectField('Sig', user.sig).byID();
+userTA.selectField("Sig", user.sig).byID();
 
 // Update a row
 userTA
-  .update('UserProfile')
+  .updateOne("UserProfile")
   .setInputs(user.name, user.sig)
   .byID();
 
 // Delete a row by ID
-userTA.deleteOne('ByID').byID();
+userTA.deleteOne("ByID").byID();
 
 export default userTA;
